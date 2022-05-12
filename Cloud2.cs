f02da026560fa11304b19b73162fe246999597a0
Jager1026 <74218548+Jager1026@users.noreply.github.com>
2022-05-12 18:43:58 -0500

    Add files via upload

---
+++ Cloud2.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Cloud2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    //public Vector3 moveDir;         // direction to move in
+    public float moveSpeed = 0.5f;         // speed to move at along moveDir
+    public float Timetolive = 15f;
+
+
+    // private float aliveTime = 8.0f; // time before object is destroyed
+    void Start()
+    {
+        Destroy(gameObject, Timetolive);
+    }
+
+    public void move()
+    {
+        //se mueve la moneda
+        gameObject.transform.position = gameObject.transform.position + new Vector3(-3, 0, 0) * Time.deltaTime * moveSpeed;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //se mueve la moneda
+        gameObject.transform.position = gameObject.transform.position + new Vector3(-3, 0, 0) * Time.deltaTime * moveSpeed;
+
+    }
+
+}
