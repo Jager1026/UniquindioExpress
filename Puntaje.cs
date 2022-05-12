5f0ed0ba8335e713982e462c632498a95585eb3a
Jager1026 <74218548+Jager1026@users.noreply.github.com>
2022-05-12 18:43:30 -0500

    Update Puntaje.cs

--- Puntaje.cs
+++ Puntaje.cs
@@ -3,34 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-public class Puntaje : MonoBehaviour
+public class score : MonoBehaviour
 {
-    public float puntos;
+    private float puntos;
     private TextMeshProUGUI textMesh;
 
     private void Start()
     {
-        //se accede al componente de la interfaz
         textMesh = GetComponent<TextMeshProUGUI>();
     }
-
-
-
-    public void agregarPuntos()
+    private void Update()
     {
-        //se añaden puntos cada segundo
-        puntos += Time.deltaTime;
-
-        //se escriben los puntos en la interfaz
         textMesh.text = puntos.ToString("0");
     }
-    // private void Update()
-    //{
-    //   puntos += Time.deltaTime;
-    //    textMesh.text = puntos.ToString("0");
-    //  }
 
-    //metodo para sumar puntos
     public void SumarPuntos(float PuntosEntrada)
     {
         puntos += PuntosEntrada;
