eb6279694699b7de011d90547c983b7301cf2788
Jager1026 <74218548+Jager1026@users.noreply.github.com>
2022-05-12 18:41:58 -0500

    Update GameManager.cs

--- GameManager.cs
+++ GameManager.cs
@@ -9,20 +9,25 @@ public class GameManager : MonoBehaviour
     public Renderer fondo;
 
     public GameObject moneda;
-
-    //private Coin coin;
+    public GameObject nube1;
+    public GameObject nube2;
 
     public float velocidadMonedas = 0.5f;
-    //public List<GameObject> monedas;
+
     public bool start;
     public bool finish;
 
     public float spawnRate;
     public float lastSpawn;
 
-    public float tiempoMision=20;
+    public float spawnRateCloud;
+    public float LastSpawnCloud;
 
-    public Puntaje puntaje; 
+    public float tiempoMision = 60;
+
+    public score puntaje;
+
+    //public Puntaje puntaje;
 
     // Start is called before the first frame update
     void Start()
@@ -30,6 +35,7 @@ public class GameManager : MonoBehaviour
         start = false;
         //instanciamos la moneda
         //moneda(Instantiate(moneda, new Vector2(10, 0),Quaternion.identity));
+
     }
 
     // Update is called once per frame
@@ -46,12 +52,12 @@ public class GameManager : MonoBehaviour
             {
                 start = true;
                 MenuStart.SetActive(false);
-            }    
+            }
         }
 
         // si la mision se termina
         if (finish == true && start == true)
-        {   
+        {
             //Control del Menu de termino de mision
             MenuComplete.SetActive(true);
 
@@ -65,7 +71,6 @@ public class GameManager : MonoBehaviour
         if (start == true && finish == false)
         {
             //llama al metodo agregarPuntos
-            puntaje.agregarPuntos();
 
             //se establece el limite de tiempo de la mision
             tiempoMision -= Time.deltaTime;
@@ -80,10 +85,24 @@ public class GameManager : MonoBehaviour
             // se generan numeros aletorios
             float randomCoins = Random.Range(-3, 5);
 
+            float randomClouds1Y = Random.Range(1.5f, 5);
+            float randomClouds2Y = Random.Range(1.5f, 5);
+
+            float randomClouds1X = Random.Range(13, 15);
+            float randomClouds2X = Random.Range(13, 15);
             //mover el fondo
             fondo.material.mainTextureOffset = fondo.material.mainTextureOffset + new Vector2(0.2f, 0) * Time.deltaTime;
 
-            //determinamos el tiempo dde respawn
+            if (Time.time - spawnRateCloud >= LastSpawnCloud)
+            {
+                LastSpawnCloud = Time.time;
+                //instanciamos las nubes
+                Instantiate(nube1, new Vector3(randomClouds1X, randomClouds1Y, 0), Quaternion.identity);
+
+                LastSpawnCloud = Time.time;
+                Instantiate(nube2, new Vector3(randomClouds2X, randomClouds2Y, 0), Quaternion.identity);
+            }
+            //determinamos el tiempo de respawn
             if (Time.time - spawnRate >= lastSpawn)
             {
                 //tiempo de gracia entre spawn de monedas
@@ -91,14 +110,15 @@ public class GameManager : MonoBehaviour
 
                 //se crea una nueva moneda en un lugar aletorio
                 Instantiate(moneda, new Vector3(10, randomCoins, 0), Quaternion.identity);
+
             }
 
-//Extreme Sport Trap Music | PISTA by Alex-Productions | https://www.youtube.com/channel/UCx0_M61F81Nfb-BRXE-SeVA
-//Music promoted by https://www.chosic.com/free-music/all/
-//Creative Commons CC BY 3.0
-//https://creativecommons.org/licenses/by/3.0/
+            //Extreme Sport Trap Music | PISTA by Alex-Productions | https://www.youtube.com/channel/UCx0_M61F81Nfb-BRXE-SeVA
+            //Music promoted by https://www.chosic.com/free-music/all/
+            //Creative Commons CC BY 3.0
+            //https://creativecommons.org/licenses/by/3.0/
 
-
+            //https://patrickdearteaga.com
         }
     }
 }
